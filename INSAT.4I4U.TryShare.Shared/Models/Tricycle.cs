45a98892f8ae4e76af1caa450b66a86a29ada9cb
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2022-11-20 13:52:27 +0100

    Add tricyle status (taken / free) + rename for clarity
    Related Work Items: #73

--- INSAT.4I4U.TryShare.Shared/Models/Tricycle.cs
+++ INSAT.4I4U.TryShare.Shared/Models/Tricycle.cs
@@ -9,14 +9,16 @@ namespace INSAT._4I4U.TryShare.Shared.Models
 {
     /// <summary>
     /// A connected tricyle.
-    /// Location is determined by Longitude and Latitude.
+    /// LastKnownLocation is determined by LastKnownLongitude and LastKnownLatitude.
     /// </summary>
     public class Tricycle
     {
         public required int ID { get; set; }
-        public required double Latitude { get; set; }
-        public required double Longitude { get; set; }
+        public required double LastKnownLatitude { get; set; }
+        public required double LastKnownLongitude { get; set; }
+        
         [Range(0, 100, ErrorMessage = "The value must be a whole percentage")]
         public required int BatteryPercentage { get; set; }
+        public required bool IsAvailable { get; set; }
     }
 }
