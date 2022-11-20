408340b99b05db8a4758873ad3119f0bc50791aa
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2022-11-20 13:39:08 +0100

    Add project files.

---
+++ INSAT.4I4U.TryShare.Shared/Models/Tricycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSAT._4I4U.TryShare.Shared.Models
+{
+    /// <summary>
+    /// A connected tricyle.
+    /// Location is determined by Longitude and Latitude.
+    /// </summary>
+    public class Tricycle
+    {
+        public required int ID { get; set; }
+        public required double Latitude { get; set; }
+        public required double Longitude { get; set; }
+        [Range(0, 100, ErrorMessage = "The value must be a whole percentage")]
+        public required int BatteryPercentage { get; set; }
+    }
+}
