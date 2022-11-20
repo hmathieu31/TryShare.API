e76f997890af8302a31a45a608e39c537ee13cd6
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2022-11-20 14:31:48 +0100

    Configure a LocalDB SQLServ
    Related Work Items: #63

--- INSAT.4I4U.TryShare.TricyclesAvailable/Program.cs
+++ INSAT.4I4U.TryShare.TricyclesAvailable/Program.cs
@@ -1,6 +1,17 @@
+using INSAT._4I4U.TryShare.TricyclesAvailable.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+// Add the DbContext to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add the database exception filter
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
