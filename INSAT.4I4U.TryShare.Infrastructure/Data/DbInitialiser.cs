c0125aeaedd5fbb641be0f402d7a42231b985d2d
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-12-15 10:36:41 +0100

    Update DbContext to work with the changed database

---
+++ INSAT.4I4U.TryShare.Infrastructure/Data/DbInitialiser.cs
@@ -0,0 +1,54 @@
+﻿using INSAT._4I4U.TryShare.Core.Models;
+
+namespace INSAT._4I4U.TryShare.Infrastructure.Data
+{
+    public class DbInitialiser
+    {
+        private readonly ApplicationDbContext context;
+
+        public DbInitialiser(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Run()
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Tricycles.Any())
+                return;
+
+            var tricycles = new List<Tricycle>
+            {
+                new Tricycle
+                {
+                    BatteryPercentage = 24,
+                    IsAvailable = false,
+                    LastKnownLatitude = 425.0,
+                    LastKnownLongitude = 1023.0
+                },
+                new Tricycle
+                {
+                    BatteryPercentage = 24,
+                    IsAvailable = false,
+                    LastKnownLatitude = 3680.9,
+                    LastKnownLongitude = 2474.2
+                }
+            };
+
+            context.Tricycles.AddRange(tricycles);
+            context.SaveChanges();
+        }
+
+        public void FreeTricycles()
+        {
+            context.Database.EnsureCreated();
+
+            foreach (var tricycle in context.Tricycles)
+            {
+                tricycle.IsAvailable = true;
+            }
+            context.SaveChanges();
+        }
+    }
+}
